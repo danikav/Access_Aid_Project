241f8b29a3d2db5e6ef1382ee05fca87cd962b63
Dani <danikav1989@gmail.com>
2021-05-27 14:48:58 +0100

    Routes added

--- src/App.js
+++ src/App.js
@@ -4,18 +4,24 @@ import Map from './components/Map';
 import NavBar from './components/NavBar';
 import Business from './components/Business';
 import SearchBar from './components/SearchBar';
+import Home from "./components/Home";
+import ErrorPage from './components/ErrorPage';
 import './App.css';
 
 function App() {
   return (
     <Router>
     <>
-        <h1>Access Aid</h1>
-        <NavBar />
-        <Business />
-        <Map />
-        <SearchBar />
-    </>
+    <NavBar />
+        <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/businesses" component={Business} />
+        <Route
+          path="/resources" 
+        />
+        <Route component={ErrorPage}/>
+        </Switch>
+        </>
     </Router>
   );
 }
---
+++ src/components/ErrorPage.js
@@ -0,0 +1,9 @@
+const ErrorPage = () => {
+
+    return(
+        <>
+        <h1>OH NO!!!</h1>
+        </>
+    )
+}
+export default ErrorPage;
