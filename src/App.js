c4b01fa85b1e9646efec9522ccf72f89da1f2e83
Dani <danikav1989@gmail.com>
2021-05-27 14:34:27 +0100

    NavBar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import ContainerBox from './containers/ContainerBox'
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import Map from './components/Map';
+import NavBar from './components/NavBar';
+import Business from './components/Business';
+import SearchBar from './components/SearchBar';
 import './App.css';
 
 function App() {
   return (
-    <ContainerBox />
+    <Router>
+    <>
+        <h1>Access Aid</h1>
+        <NavBar />
+        <Business />
+        <Map />
+        <SearchBar />
+    </>
+    </Router>
   );
 }
 
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,29 @@
+import { Link, useHistory } from "react-router-dom";
+
+
 const NavBar = () => {
+  const history = useHistory();
 
+  const goBack = () => {
+    history.goBack();
+  }
 
-    return(
-        <h1>Nav Bar</h1>
-    )
+  return (
+    <ul>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="/businesses">Businesses</Link>
+      </li>
+      <li>
+        <Link to="/resources">Resources</Link>
+      </li>
+      <li>
+        <button onClick={goBack}>Back</button>
+      </li>
+    </ul>
+  );
 }
-export default NavBar; 
+
+export default NavBar;
