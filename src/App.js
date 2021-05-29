faaf81a24029d4b6bd0682b915ada76309b6d2ea
alexkinsey <alexanderkinsey@icloud.com>
2021-05-29 11:56:25 +0100

    Added css container to homepage to push content from under navbar. This may need to be done on all pages

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import NavBar from './components/NavBar';
 import Business from './components/Business';
-import SearchBar from './components/SearchBar';
 import Home from './components/Home';
 import ErrorPage from './components/ErrorPage';
 import Resources from './components/Resources'
@@ -13,7 +12,6 @@ function App() {
     <>
     <Router>
       <NavBar className="nav-bar" />
-      <SearchBar />
       <Switch>
         <Route exact path="/" component={Home} />
         <Route path="/businesses" component={Business} />
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,15 @@
 import MapContainer from './MapContainer';
+import SearchBar from './SearchBar';
+import './Home.css'
 
 const Home = () => {
   return (
-    <>
+    <div className="home-container">
       <h1>Access Aid</h1>
       <h3>Travel aid for the differently abled</h3>
       <MapContainer />
-    </>
+      <SearchBar />
+    </div>
   );
 };
 export default Home;
