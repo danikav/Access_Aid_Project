8d5b6da976e6c37b0552f945fd993fe7e9282078
SinnottTM <tim.sinnott86@gmail.com>
2021-05-29 16:48:50 +0100

    8th commit - developing classes

--- server/src/main/java/com/codeclan/server/models/Location.java
+++ server/src/main/java/com/codeclan/server/models/Location.java
@@ -4,6 +4,8 @@ package com.codeclan.server.models;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import javax.persistence.*;
+import javax.swing.*;
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
@@ -19,6 +21,9 @@ public class Location {
 
     @Column(name = "type")
     private String type;
+
+    @Column(name = "image")
+    private Icon picture;
 
     @Column(name = "longitude")
     private int longitude;
@@ -50,13 +55,17 @@ public class Location {
     @Column(name = "total_score")
     private int total_score;
 
+    @Column(name = "user_reviews")
+    private ArrayList <String> user_reviews;
+
     @JsonIgnoreProperties({"location"})
     @OneToMany(mappedBy = "location", fetch = FetchType.LAZY)
     private List<User> users;
 
-    public Location(String name, String type, int longitude, int latitude, String description, boolean seating, int high_light_score, int low_noise_score, int adequate_space, int mobility_access, int staff_support, int total_score) {
+    public Location(String name, String type, Icon picture, int longitude, int latitude, String description, boolean seating, int high_light_score, int low_noise_score, int adequate_space, int mobility_access, int staff_support, int total_score, ArrayList <String> user_reviews) {
         this.name = name;
         this.type = type;
+        this.picture = picture;
         this.longitude = longitude;
         this.latitude = latitude;
         this.description = description;
@@ -67,7 +76,8 @@ public class Location {
         this.mobility_access = mobility_access;
         this.staff_support = staff_support;
         this.total_score = total_score;
-
+        this.user_reviews = user_reviews;
+        this.users = new ArrayList<>();
 
     }
 
