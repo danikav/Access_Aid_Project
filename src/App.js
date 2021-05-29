91a468557c99d2bc19522c1833a8bba857bac440
alexkinsey <alexanderkinsey@icloud.com>
2021-05-29 12:48:44 +0100

    corrected navbar overlap

--- src/App.js
+++ src/App.js
@@ -4,22 +4,23 @@ import NavBar from './components/NavBar';
 import Business from './components/Business';
 import Home from './components/Home';
 import ErrorPage from './components/ErrorPage';
-import Resources from './components/Resources'
+import Resources from './components/Resources';
 import './App.css';
 
 function App() {
   return (
     <>
-    <Router>
-      <NavBar className="nav-bar" />
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/businesses" component={Business} />
-        <Route path="/resources" component={Resources}/>
-        <Route component={ErrorPage} />
-      </Switch>
-    </Router>
-    
+      <Router>
+        <NavBar className="nav-bar" />
+        <div className="page-content">
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/businesses" component={Business} />
+            <Route path="/resources" component={Resources} />
+            <Route component={ErrorPage} />
+          </Switch>
+        </div>
+      </Router>
     </>
   );
 }
