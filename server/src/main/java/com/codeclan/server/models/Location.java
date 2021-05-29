1a1262ef6eeb8f2062ddd745c48e3ffd24face0a
SinnottTM <tim.sinnott86@gmail.com>
2021-05-29 16:50:21 +0100

    9th commit - location class alpha build

--- server/src/main/java/com/codeclan/server/models/Location.java
+++ server/src/main/java/com/codeclan/server/models/Location.java
@@ -83,4 +83,132 @@ public class Location {
 
     public Location() {
     }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Icon getPicture() {
+        return picture;
+    }
+
+    public void setPicture(Icon picture) {
+        this.picture = picture;
+    }
+
+    public int getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(int longitude) {
+        this.longitude = longitude;
+    }
+
+    public int getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(int latitude) {
+        this.latitude = latitude;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isSeating() {
+        return seating;
+    }
+
+    public void setSeating(boolean seating) {
+        this.seating = seating;
+    }
+
+    public int getHigh_light_score() {
+        return high_light_score;
+    }
+
+    public void setHigh_light_score(int high_light_score) {
+        this.high_light_score = high_light_score;
+    }
+
+    public int getLow_noise_score() {
+        return low_noise_score;
+    }
+
+    public void setLow_noise_score(int low_noise_score) {
+        this.low_noise_score = low_noise_score;
+    }
+
+    public int getAdequate_space() {
+        return adequate_space;
+    }
+
+    public void setAdequate_space(int adequate_space) {
+        this.adequate_space = adequate_space;
+    }
+
+    public int getMobility_access() {
+        return mobility_access;
+    }
+
+    public void setMobility_access(int mobility_access) {
+        this.mobility_access = mobility_access;
+    }
+
+    public int getStaff_support() {
+        return staff_support;
+    }
+
+    public void setStaff_support(int staff_support) {
+        this.staff_support = staff_support;
+    }
+
+    public int getTotal_score() {
+        return total_score;
+    }
+
+    public void setTotal_score(int total_score) {
+        this.total_score = total_score;
+    }
+
+    public ArrayList<String> getUser_reviews() {
+        return user_reviews;
+    }
+
+    public void setUser_reviews(ArrayList<String> user_reviews) {
+        this.user_reviews = user_reviews;
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
 }
