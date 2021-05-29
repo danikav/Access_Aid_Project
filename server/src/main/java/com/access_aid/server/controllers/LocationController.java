28c9989addcb84edfc2135bc4072d24e780d632b
SinnottTM <tim.sinnott86@gmail.com>
2021-05-29 17:39:25 +0100

    12th commit - controllers started

--- server/src/main/java/com/access_aid/server/controllers/LocationController.java
+++ server/src/main/java/com/access_aid/server/controllers/LocationController.java
@@ -1,4 +1,21 @@
 package com.access_aid.server.controllers;
 
+import com.access_aid.server.repositories.LocationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.swing.text.html.parser.Entity;
+
+@RestController
 public class LocationController {
+
+    @Autowired
+    LocationRepository locationRepository;
+
+//    @GetMapping(value = "/locations")
+//    public ResponseEntity getAllLocations{
+//        return new Entity()
+//    }
 }
--- server/src/main/java/com/access_aid/server/repositories/UserRepository.java
+++ server/src/main/java/com/access_aid/server/repositories/UserRepository.java
@@ -1,4 +1,7 @@
 package com.access_aid.server.repositories;
 
-public interface UserRepository {
+import com.access_aid.server.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
 }
