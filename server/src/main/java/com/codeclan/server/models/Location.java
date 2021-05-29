9383211e3e19952dd4594a055f4b3d748d79dac4
SinnottTM <tim.sinnott86@gmail.com>
2021-05-29 16:32:38 +0100

    5th commit - developing classes

--- server/src/main/java/com/codeclan/server/models/Location.java
+++ server/src/main/java/com/codeclan/server/models/Location.java
@@ -32,21 +32,30 @@ public class Location {
     @Column(name = "seating")
     private boolean seating;
 
-    @Column(name = "light_score")
-    private int light_score;
+    @Column(name = "high_light_score")
+    private int high_light_score;
+
+    @Column(name = "low_noise_score")
+    private int low_noise_score;
+
+    @Column(name = "adequate_space")
+    private int adequate_space;
 
     @JsonIgnoreProperties({"location"})
     @OneToMany(mappedBy = "location", fetch = FetchType.LAZY)
     private List<User> users;
 
-    public Location(String name, String type, int longitude, int latitude, String description, boolean seating, int light_score) {
+    public Location(String name, String type, int longitude, int latitude, String description, boolean seating, int high_light_score, int low_noise_score, int adequate_spacee) {
         this.name = name;
         this.type = type;
         this.longitude = longitude;
         this.latitude = latitude;
         this.description = description;
         this.seating = seating;
-        this.light_score = light_score;
+        this.high_light_score = high_light_score;
+        this.low_noise_score = low_noise_score;
+        this.adequate_space = adequate_spacee;
+
 
     }
 
