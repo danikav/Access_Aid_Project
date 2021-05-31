eb61e9d7b96c67430bc50984ecd3ca8f6ab3e39d
alexkinsey <alexanderkinsey@icloud.com>
2021-05-31 16:02:57 +0100

    markers now load on map. Switching to map branch for further development

--- src/components/MapContainer.js
+++ src/components/MapContainer.js
@@ -3,25 +3,30 @@ import { Map, Marker } from 'pigeon-maps';
 import PopUp from './PopUp';
 import './Map.css';
 
-const MapContainer = ({locations, locationsLoaded}) => {
+const MapContainer = ({ locations, locationsLoaded }) => {
   const [popUpWindow, setPopUpWindow] = useState(false);
+  const [slecetedMarker, setSlecetedMarker] = useState(0);
 
   if (!locationsLoaded) {
-    return <p>loading data...</p>
+    return <p>Connecting to map data...</p>;
   }
 
-  const latitude = locations[0].latitude
-  const longitude = locations[0].longitude
-  const cordenients = [latitude, longitude]
-
-  const togglePop = () => {
+  const togglePop = (id) => {
     setPopUpWindow(!popUpWindow);
   };
+
+  const MarkerNode = locations.map((location) => {
+    const latitude = location.latitude;
+    const longitude = location.longitude;
+    const coordinates = [longitude, latitude];
+
+    return <Marker width={30} anchor={coordinates} onClick={togglePop} key={location.id} />;
+  });
 
   return (
     <div className="map-container">
       <Map className="map" defaultCenter={[55.9467749, -3.2020009]} defaultZoom={11}>
-        <Marker width={50} anchor={[55.9367749, -3.2020009]} onClick={togglePop} />
+        {MarkerNode}
       </Map>
       {popUpWindow ? <PopUp className="popup" toggle={togglePop}></PopUp> : null}
     </div>
