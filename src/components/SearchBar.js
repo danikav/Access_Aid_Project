839f2927b39fbc3da1a11c7bec5d9151eaa443ce
Dani <danikav1989@gmail.com>
2021-05-31 10:01:01 +0100

    added refinements and search button

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,14 +1,30 @@
+import './SearchBar.css'
+
 const SearchBar = () => {
 
         const BarStyling = {width:"20rem",background:"#F2F1F9", border:"none", padding:"0.5rem"};
         return (
+          <>
+          <form class="formcontainer">
           <input 
            style={BarStyling}
            key="random1"
+           class="searchbar"
         //    value={keyword}
            placeholder={"What are you looking for?"}
         //    onChange={(e) => setKeyword(e.target.value)}
           />
+          <select class="refinements">
+    <option value="" disabled selected>Choose Refinements</option>
+    <option value="1">Visual Impairment</option>
+    <option value="2">Physical Impairment</option>
+    <option value="3">Example</option>
+    <option value="4">Example1</option>
+    <option value="5">Example2</option>
+  </select>
+          <button type="submit" class="searchbutton">Search</button>
+          </form>
+          </>
         );
       }
      
