a9ca56d72c89a0912b713b13dd7498f2147b5525
SinnottTM <tim.sinnott86@gmail.com>
2021-05-31 08:39:45 +0100

    15th commit - controllers major update

--- server/src/main/java/com/access_aid/server/controllers/LocationController.java
+++ server/src/main/java/com/access_aid/server/controllers/LocationController.java
@@ -1,12 +1,13 @@
 package com.access_aid.server.controllers;
 
+import com.access_aid.server.models.Location;
 import com.access_aid.server.repositories.LocationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import javax.swing.text.html.parser.Entity;
+import java.util.List;
 
 @RestController
 public class LocationController {
@@ -14,8 +15,33 @@ public class LocationController {
     @Autowired
     LocationRepository locationRepository;
 
-//    @GetMapping(value = "/locations")
-//    public ResponseEntity getAllLocations{
-//        return List<Location> locationRepository.findAll();
-//    }
+//
+    @GetMapping(value = "/locations")
+    public ResponseEntity <List<Location>> getAllLocations(){
+        return new ResponseEntity<>(locationRepository.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/locations/{id}")
+    public ResponseEntity getLocation(@PathVariable Long id){
+        return new ResponseEntity<>(locationRepository.findById(id), HttpStatus.OK);
+    }
+
+    @PostMapping(value = "/locations")
+    public ResponseEntity<Location> postLocation(@RequestBody Location location){
+        locationRepository.save(location);
+        return new ResponseEntity<>(location, HttpStatus.CREATED);
+    }
+
+    @GetMapping(value = "/locations/delete")
+    public ResponseEntity<Location> deleteLocation(@RequestBody Location location){
+        locationRepository.delete(location);
+        return new ResponseEntity<>(location, HttpStatus.GONE);
+    }
+
+    @GetMapping(value = "/locations/delete{id}")
+    public ResponseEntity deleteLocationById(@PathVariable Long id){
+        locationRepository.deleteById(id);
+        return new ResponseEntity<>(locationRepository.findById(id), HttpStatus.GONE);
+    }
+
 }
--- server/src/main/java/com/access_aid/server/repositories/LocationRepository.java
+++ server/src/main/java/com/access_aid/server/repositories/LocationRepository.java
@@ -3,9 +3,27 @@ package com.access_aid.server.repositories;
 import com.access_aid.server.models.Location;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface LocationRepository extends JpaRepository<Location, Long>{
 
-    List<Location> findAll();
+//    Pre-set generic CRUD operations for use in controller (Auto with JpaRepo)
+
+//    SAVE/UPDATE
+//    <S extends T> S save(S entity);
+//    NB. CrudRepository has only save but it acts as update as well if used on taken ID
+
+//    INDEX
+//    T findOne(ID primaryKey);
+//
+//    GET
+//    Iterable<T> findAll();
+//
+//    Find length of
+//    Long count();
+//
+//    DELETE
+//    void delete(T entity);
+//
+//    Exists? True/False check
+//    boolean exists(ID primaryKey);
+
 }
