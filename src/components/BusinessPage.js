3d451ccb00f495b251a02c13b3607245fd62ff88
alexkinsey <alexanderkinsey@icloud.com>
2021-05-31 11:05:49 +0100

    added business page

---
+++ src/components/BusinessPage.js
@@ -0,0 +1,21 @@
+function BusinessPage() {
+  return (
+    <div className="business">
+      <img className="business-image" src="https://wwwcdn.ithaca.edu/sites/default/files/styles/max_2600x2600/public/2020-08/edinburgh.jpeg?itok=t_48rAQh"></img>
+      <div className="business-info">
+        <h2>Title of Business</h2>
+        <p>Rating: &#9733;&#9733;&#9733;&#9733;&#9734;</p>
+        <p>Description of business here. This may be a few sentences long.</p>
+        <ul>
+          <li>Star rating here.</li>
+          <li>Star rating here.</li>
+          <li>Star rating here.</li>
+          <li>Star rating here.</li>
+          <li>Star rating here.</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default BusinessPage;
