2b09e60b49726f891e7d54d76548453216f7d14e
alexkinsey <alexanderkinsey@icloud.com>
2021-06-02 14:54:59 +0100

    created profile icon

--- src/components/ReviewListItem.jsx
+++ src/components/ReviewListItem.jsx
@@ -1,19 +1,49 @@
-import Rating from 'react-rating'
+import Rating from 'react-rating';
+import './Reviews.css';
 
-const ReviewListItem = ({name, comment, totalScore, staffSupport, mobilityAccess, lowNoiseScore, highLightScore, adequateSpaceScore}) => {
+const ReviewListItem = ({ name, comment, totalScore, staffSupport, mobilityAccess, lowNoiseScore, highLightScore, adequateSpaceScore, date }) => {
+  const firstLetter = name.substring(0, 1)
+  
+  return (
+    <div className="review-item">
+      <h4 className="review-title">
+        <div className="display-icon">{firstLetter}</div>
+        {name} <Rating className="rating" emptySymbol={<span>&#9734;</span>} fullSymbol={<span>&#9733;</span>} initialRating={totalScore} fractions={2} readonly />
+        <span className="review-date"> {date}</span>
+      </h4>
+      <p>"{comment}"</p>
+      <div>
+        Staff Support
+        <div>
+          <Rating className="rating" emptySymbol={<span>&#9734;</span>} fullSymbol={<span>&#9733;</span>} initialRating={staffSupport} fractions={2} readonly />
+        </div>
+      </div>
+      <div>
+        Mobility Access
+        <div>
+          <Rating className="rating" emptySymbol={<span>&#9734;</span>} fullSymbol={<span>&#9733;</span>} initialRating={mobilityAccess} fractions={2} readonly />
+        </div>
+      </div>
+      <div>
+        Low Noise
+        <div>
+          <Rating className="rating" emptySymbol={<span>&#9734;</span>} fullSymbol={<span>&#9733;</span>} initialRating={lowNoiseScore} fractions={2} readonly />
+        </div>
+      </div>
+      <div>
+        High Light
+        <div>
+          <Rating className="rating" emptySymbol={<span>&#9734;</span>} fullSymbol={<span>&#9733;</span>} initialRating={highLightScore} fractions={2} readonly />
+        </div>
+      </div>
+      <div>
+        Adequate Space
+        <div>
+          <Rating className="rating" emptySymbol={<span>&#9734;</span>} fullSymbol={<span>&#9733;</span>} initialRating={adequateSpaceScore} fractions={2} readonly />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-  return(
-    <>
-    <h4>{name}</h4>
-    <div>Overall Score: <Rating className="rating" emptySymbol={<span>&#9734;</span>} fullSymbol={<span>&#9733;</span>} initialRating={totalScore} fractions={2} readonly /></div>
-    <p>{comment}</p>
-    <div>Staff Support: <Rating className="rating" emptySymbol={<span>&#9734;</span>} fullSymbol={<span>&#9733;</span>} initialRating={staffSupport} fractions={2} readonly /> 
-    Mobility Access: <Rating className="rating" emptySymbol={<span>&#9734;</span>} fullSymbol={<span>&#9733;</span>} initialRating={mobilityAccess} fractions={2} readonly /> 
-    Low Noise Score: <Rating className="rating" emptySymbol={<span>&#9734;</span>} fullSymbol={<span>&#9733;</span>} initialRating={lowNoiseScore} fractions={2} readonly /> 
-    High Light Score: <Rating className="rating" emptySymbol={<span>&#9734;</span>} fullSymbol={<span>&#9733;</span>} initialRating={highLightScore} fractions={2} readonly /> 
-    Adequate Space Score: <Rating className="rating" emptySymbol={<span>&#9734;</span>} fullSymbol={<span>&#9733;</span>} initialRating={adequateSpaceScore} fractions={2} readonly /></div>
-    </>
-  )
-}
-
-export default ReviewListItem
+export default ReviewListItem;
