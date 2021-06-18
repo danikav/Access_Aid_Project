9731946b131db2f4fd5d9f2625a148c23a79677c
alexkinsey <alexanderkinsey@icloud.com>
2021-06-18 13:57:52 +0100

    added planning

--- src/container/HomeContainer.js
+++ src/container/HomeContainer.js
@@ -46,7 +46,7 @@ const Home = ({ locations, locationsLoaded }) => {
     <div className="home-container">
       <img src={Logo} alt="logo" className="logo"></img>
       <h1 className="title">Access Aid</h1>
-      <h3>Travel aid for the differently abled</h3>
+      <h3>Empowering you to access your community</h3>
       <SearchBar handleSubmit={handleSearchSubmit} />
       <MapComponent locations={filteredLocations} locationsLoaded={locationsLoaded} />
     </div>
